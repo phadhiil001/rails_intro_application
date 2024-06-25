6dab4b68552258043a984dce23d678a37253d800
phadhiil001 <phadhiil001@gmail.com>
2024-06-25 01:36:38 -0500

    Added all the controllers 1

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,47 @@
+class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    if @ticket.update(ticket_params)
+      redirect_to @ticket
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    redirect_to tickets_path
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:event_id, :user_id, :ticket_type, :price)
+  end
+end
---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,47 @@
+class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to @venue
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    if @venue.update(venue_params)
+      redirect_to @venue
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    redirect_to venues_path
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :capacity)
+  end
+end
