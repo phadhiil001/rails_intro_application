ceecbea017505fff50c5e54ba489ef496ab0f290
phadhiil001 <phadhiil001@gmail.com>
2024-07-01 20:36:24 -0500

    Added Active admin to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+
+    def self.ransackable_associations(auth_object = nil)
+        ["events", "tickets"]
+      end
+
+      def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "email", "id", "id_value", "name", "role", "updated_at"]
+      end
+
     # Association include
     has_many :events
     has_many :tickets
