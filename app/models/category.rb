2592304309e3500ebcc3b182dcf938de635961de
phadhiil001 <phadhiil001@gmail.com>
2024-07-01 20:38:22 -0500

    Added Active admin to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
+
+    def self.ransackable_associations(auth_object = nil)
+        ["categorizations", "events"]
+      end
+
+      def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "id", "id_value", "name", "updated_at"]
+      end
     # Association include
     has_many :categorizations
     has_many :events, through: :categorizations
