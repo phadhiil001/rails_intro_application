14fad87a7f69f5348d898546b675c6b5f946da48
phadhiil001 <phadhiil001@gmail.com>
2024-07-01 20:37:14 -0500

    Added Active admin to venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,12 @@
 class Venue < ApplicationRecord
+
+    def self.ransackable_associations(auth_object = nil)
+        ["events"]
+      end
+
+      def self.ransackable_attributes(auth_object = nil)
+        ["address", "capacity", "created_at", "id", "id_value", "name", "updated_at"]
+      end
     # Association include
     has_many :events
 
